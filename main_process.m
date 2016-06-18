aab1d009b65602431b0d19e4e07cdc0cbb63d342
zejia <buttonzhengze@hotmail.com>
2016-06-18 14:41:36 -0700

    get image now can handle multiple Z areas

--- main_process.m
+++ main_process.m
@@ -3,7 +3,7 @@ path(path, 'data/backgrounds');
 path(path, 'data/foregrounds');
 
 
-% if z_area_num = 2; then z_neuron_num = [5, 25] TM, LM
+% z_neuron_num = [5, 25] TM, LM; z_neuron_num = [25] LM only
 z_neuron_num = [25];
 y_neuron_num = 4;
 input_dim = [18, 18];
@@ -23,8 +23,25 @@ if(training_flag)
         % motors both
         % get_image need to use figure out the stride of location based on
         % input_dim
-        [training_image, true_pos, true_type] = get_image(input_dim, z_neuron_num);
-        dn_learn(dn, training_image, true_pos, true_type);        
+        % true_z is the vector containing the maximum index of neurons in
+        % each area, e.g true_z = [1,2] means that type 1 is at location 2
+        % true_z = [3] means that the foreground is at location 3
+        [training_image, true_z] = get_image(input_dim, z_neuron_num);
+        dn = dn_learn(dn, training_image, true_z);
+        
+        if (check_splitting(dn))
+            dn = dn_split(dn);
+        end
     end
 end
 
+testing_num = 1000;
+if(testing_flag)
+    type_error = 0;
+    location_error = 0;
+    
+    [testing_image, true_z] = get_image(input_dim, z_neuron_num);
+    
+    % z_output is the vector with all maximum index of areas in Z
+    z_output = dn_test(dn, testing_image);
+end
