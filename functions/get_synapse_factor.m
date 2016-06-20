f90406cd4944c8f32d3953fab4f5119fe22fb236
zejia <buttonzhengze@hotmail.com>
2016-06-19 20:42:24 -0700

    "currently synapse factor not working"

--- functions/get_synapse_factor.m
+++ functions/get_synapse_factor.m
@@ -4,9 +4,9 @@ function synapse_factor = get_synapse_factor(synapse_diff, synapse_factor, ...
 % 1: compute the mean of the entire synapse_diff
 % 2: only compute the mean of the synapse_diff where the corresponding
 % synapse_factor is greater than 0
-% we implement the 2nd situation
-% current_diff = synapse_diff(synapse_factor > 0);
+% we implement the 1st situation
 
+%current_diff = synapse_diff(synapse_factor > 0);
 current_diff = synapse_diff;
 mean_diff = mean(current_diff);
 lower_thresh = synapse_coefficient(1) * mean_diff;
@@ -15,13 +15,13 @@ upper_thresh = synapse_coefficient(2) * mean_diff;
 
 % TODO: added in synapse age!
 synapse_factor(synapse_diff > upper_thresh) = 0;
-%synapse_factor(synapse_diff < lower_thresh) = 1;
+synapse_factor(synapse_diff < lower_thresh) = 1;
 
 % TODO: add in grow method
-synapse_factor(synapse_diff <= upper_thresh) = 1./ ... 
-    (synapse_diff(synapse_diff <= upper_thresh)+1/(256*sqrt(12))) - ...
-    1/(upper_thresh +1/(256*sqrt(12)));
+synapse_factor((synapse_diff <= upper_thresh).* (synapse_diff >= lower_thresh) == 1) = ...
+    (synapse_factor((synapse_diff <= upper_thresh).* (synapse_diff >= lower_thresh) == 1) - upper_thresh)/...
+    (lower_thresh - upper_thresh);
 
 if max(synapse_factor) ~= 0
-    synapse_factor = synapse_factor / max(synapse_factor);
+    synapse_factor = synapse_factor;
 end
