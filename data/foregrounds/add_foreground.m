98180f082ac7b38329674ecc67aaa717863e82b7
zejia <buttonzhengze@hotmail.com>
2016-07-02 15:39:33 -0700

    fixed get image to multiple foreground images

--- data/foregrounds/add_foreground.m
+++ data/foregrounds/add_foreground.m
@@ -1,41 +1,34 @@
-function [image,type,position]=add_foreground(background,training_type) 
-  type=0; % 01:cat; 02:dog; 03:elephant; 04:pig; 05:truck
-  listdata = 'foregrounds_to_use.txt';
-  filedata = textread(listdata,'%s','delimiter','\n','whitespace','');
-  format = char(filedata(1));
-  nImg = size(filedata, 1) - 1;
-  curr_num = 2;
-   [img,map,alpha] = imread(strcat('', char(filedata(curr_num)), '.', format),format);
-    img = double(img);
-    alpha = double(alpha > 0);
-    
-    if(training_type)
-        fg = textscan( char(filedata{curr_num}), '%s%s','delimiter', '_');
-        class = char(fg{1});
-        type=str2num(class);
-    end
- 
-   objWidth=size(img,1);
-   
-    p_r=ceil(rand*5);
-    p_c=ceil(rand*5);
-    position=(p_r-1)*5+p_c;
-    r=1+(p_r-1)*2;
-    c=1+(p_c-1)*2;
-   alpmin=min(min(alpha));
-   alpmax=max(max(alpha));
-   alpha=(alpha-alpmin)./(alpmax-alpmin);
-   img = img.*alpha;                                               %paste                                                         
-   imgmin=min(min(img));
-   imgmax=max(max(img));
-   img=(img-imgmin)./(imgmax-imgmin);
+function [image,type,position]=add_foreground(background,training_type)
+% 01:cat; 02:dog; 03:elephant; 04:pig; 05:truck
+listdata = 'foregrounds_to_use.txt';
+filedata = textread(listdata,'%s','delimiter','\n','whitespace','');
+format = char(filedata(1));
+type = ceil(rand() * training_type)+1;
+[img,~,alpha] = imread(strcat('', char(filedata(type)), '.', format),format);
+img = double(img);
+alpha = double(alpha > 0);
+
+objWidth=size(img,1);
+
+p_r=ceil(rand*5);
+p_c=ceil(rand*5);
+position=(p_r-1)*5+p_c;
+r=1+(p_r-1)*2;
+c=1+(p_c-1)*2;
+alpmin=min(min(alpha));
+alpmax=max(max(alpha));
+alpha=(alpha-alpmin)./(alpmax-alpmin);
+img = img.*alpha;                                               %paste
+imgmin=min(min(img));
+imgmax=max(max(img));
+img=(img-imgmin)./(imgmax-imgmin);
 
 % background(r:r+objWidth-1, c:c+objWidth-1)=img;   %TODO: bounds checking
-  temp = background(r:r+objWidth-1, c:c+objWidth-1);
-  temp = (1-alpha).*temp+alpha.*img;                                %paste
+temp = background(r:r+objWidth-1, c:c+objWidth-1);
+temp = (1-alpha).*temp+alpha.*img;                                %paste
 % temp = img;
 
-  background(r:r+objWidth-1, c:c+objWidth-1)=temp;
-% Vectorize the geneerated sample
- image=background;
+background(r:r+objWidth-1, c:c+objWidth-1)=temp;
+% vectorize the geneerated sample
+image=background;
 end
--- data/get_image.m
+++ data/get_image.m
@@ -5,23 +5,27 @@ function [training_image, true_z] = get_image(input_dim, z_neuron_num)
 epsilon_error = 1/(256*sqrt(12));
 background_width=input_dim(1);
 background_height=input_dim(2);
-training_type=1;
+
  
 bg=get_background(background_height,background_width);
  
 
 if(numel(z_neuron_num) == 1)
-    training_type=0;
+    training_type=1;
+else
+    % we are generating this number of types according to the number of
+    % type neurons in Z area
+    training_type = z_neuron_num(1);
 end
+
 [training_image,type,position]=add_foreground(bg,training_type);
  
-if(type>0)
+if(numel(z_neuron_num) ~= 1)
     true_z=zeros(1,2);
     true_z(1)=type;
     true_z(2)=position;
 else
     true_z=position;
 end
-%   figure;
-%   imagesc(training_image);
+
 end
